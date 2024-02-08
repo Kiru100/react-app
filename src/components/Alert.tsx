5a46be887bed228a73600373dcad31ab606ea015
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-02-08 21:35:47 +0800

    Update Alert.tsx

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,14 +2,19 @@ import React, { ReactNode } from 'react';
 
 interface Props{
     children: ReactNode;
-    setAlertVisible: (is_alert_visible: boolean)=>void;
+    onClose: ()=> void;
 }
 
-export default function Alert({children, setAlertVisible}: Props) {
+export default function Alert({children, onClose}: Props) {
   return (
     <div className="alert alert-primary alert-dismissible fade show" role="alert">
         {children}
-        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={()=>setAlertVisible(false)}></button>
+        <button 
+            type="button" 
+            className="btn-close" 
+            data-bs-dismiss="alert" 
+            aria-label="Close" 
+            onClick={onClose}></button>
     </div>
   )
 }
