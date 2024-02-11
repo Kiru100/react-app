1949e1479ca05d4ca7dac4d92f1b08b90f73e56f
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-02-11 20:09:42 +0800

    Create ListGroup.tsx

---
+++ src/components/ListGroup/ListGroup.tsx
@@ -0,0 +1,36 @@
+import './ListGroup.css';
+
+import { MouseEvent, useState } from "react";
+interface ListGroupProps {
+    items: string[];
+    heading: string;
+    onSelectedItem: (item:string) => void;
+}
+
+function ListGroup({items, heading, onSelectedItem}: ListGroupProps){
+    const [ selected_index, setSelectIndex] = useState(-1);
+    
+    return (
+        <>
+            <h1>{heading}</h1>
+            <ul className="list-group">
+                {Boolean(!items.length) && <p>No item found</p>}
+                {items.map((item, index)=> 
+                    <li 
+                        className={`list-group-item ${Boolean(selected_index === index) && "active"}`} 
+                        key={index + item} 
+                        style={{cursor:"pointer"}}
+                        onClick={()=>{
+                            setSelectIndex(index);
+                            onSelectedItem(item);
+                        } }
+                    >
+                            {item}                        
+                    </li>
+                )}
+            </ul>
+        </>
+    )
+}
+
+export default ListGroup;
