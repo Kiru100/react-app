b26bd153d3467c35a6eda2a86d752c909a342d5e
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-02-13 21:09:40 +0800

    Update ListGroup.tsx

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -7,9 +7,15 @@ const List = styled.ul`
     list-style: none;
     padding: 0;
 `
-const ListItem = styled.li`
+const ListItem = styled.li<ListItemProps>`
     padding: 5px 0;
+    background: ${props => props.active ? "blue" : "none"};
+    color: ${props => props.active ? "white" : "black"}
 `
+
+interface ListItemProps{
+    active: boolean;
+}
 
 interface ListGroupProps {
     items: string[];
@@ -18,7 +24,7 @@ interface ListGroupProps {
 }
 
 function ListGroup({items, heading, onSelectedItem}: ListGroupProps){
-    const [ selected_index, setSelectIndex] = useState(-1);
+    const [ selected_index, setSelectIndex] = useState(0);
     
     return (
         <>
@@ -27,6 +33,7 @@ function ListGroup({items, heading, onSelectedItem}: ListGroupProps){
                 {Boolean(!items.length) && <p>No item found</p>}
                 {items.map((item, index)=> 
                     <ListItem 
+                        active={selected_index === index}
                         className={`list-group-item ${Boolean(selected_index === index) && "active"}`} 
                         key={index + item} 
                         style={{cursor:"pointer"}}
