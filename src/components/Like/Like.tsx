5beea73b048cb996ee265e07d17eadd8b8ecd97c
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-02-15 12:28:52 +0800

    Update Like.tsx

--- src/components/Like/Like.tsx
+++ src/components/Like/Like.tsx
@@ -12,22 +12,26 @@ interface LikeProps {
 export default function Like({onClick}: LikeProps) {
     const [is_like_button_active, setLikeButtonActive] = useState(false);
 
+    const toggleActiveButton = () =>{
+        setLikeButtonActive(!is_like_button_active);
+        onClick();
+    }
 
     return (
-        <div onClick={onClick}>
+        <div>
             {is_like_button_active ?
                 (
-                    <span className="icon" onClick={()=>setLikeButtonActive(false)}>
-                        <FaRegHeart/>
+
+                    <span className="icon" onClick={toggleActiveButton}>
+                        <FaHeart style={{color:"#E96876"}}/>
                     </span>
                 )
                 :
                 (
-                    <span className="icon" onClick={()=>setLikeButtonActive(true)}>
-                        <FaHeart style={{color:"#E96876"}}/>
+                    <span className="icon" onClick={toggleActiveButton}>
+                        <FaRegHeart/>
                     </span>
-                )
-                  
+                )                
             }
         </div>
     )
